39e1cf601fd158f4e84b46fba5a42354add9a2f4
Quentin Kaiser <kaiserquentin@gmail.com>
2015-01-23 14:10:13 +0100

    Will be better with the script itself.

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import argparse
+
+sys.path.append('./core/')
+import base
+
+
+def main(arguments):
+
+    x = base.AAAP(arguments.apk)
+    x.analyze(arguments)
+
+description = '%%(prog)s - %s %s' % (base.__author__, base.__email__)
+parser = argparse.ArgumentParser(description=description, version=base.__version__)
+parser.add_argument("apk")
+parser.add_argument("--module")
+args = parser.parse_args()
+main(args)
