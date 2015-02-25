55df4e8cd08924e8eeb754232e31d868f7ac3aec
Quentin Kaiser <kaiserquentin@gmail.com>
2015-02-25 12:29:27 +0100

    modules/static/storage/sqlite.py
    Better output reporting.

--- modules/static/storage/sqlite.py
+++ modules/static/storage/sqlite.py
@@ -21,7 +21,7 @@ class Module(framework.module):
 
         logs = ""
         vulnerabilities = []
-        results = []
+        results = {}
 
         d = dvm.DalvikVMFormat(self.apk.get_dex())
         dx = VMAnalysis(d)
@@ -42,10 +42,16 @@ class Module(framework.module):
             if self.apk.get_package() in method.get_class_name().replace("/", "."):
                 ms = decompile.DvMethod(mx)
                 ms.process()
-                results.append({
-                    "file": method.get_class_name()[1:-1],
-                    "line": method.get_debug().get_line_start()
-                })
+                if method.get_class_name()[1:-1] not in results:
+                    results[method.get_class_name()[1:-1]] = []
+
+                if method.get_debug().get_line_start() not in \
+                        [x["line"] for x in results[method.get_class_name()[1:-1]]]:
+                    results[method.get_class_name()[1:-1]].append(
+                        {
+                            "line": method.get_debug().get_line_start()
+                        }
+                    )
 
         return {
             "results": results,
