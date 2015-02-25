32451d9a23dbb3df3eaa3e0317a23188915638c3
Quentin Kaiser <kaiserquentin@gmail.com>
2015-02-25 12:17:34 +0100

    Performance improvements. Application launch with Activity Manager. Logcat and network capture fix.

--- core/base.py
+++ core/base.py
@@ -75,54 +75,98 @@ class AAAP(framework.module):
         self.load_modules()
 
     def on_boot(self, avd):
-        self.verbose("AVD booted ma gueule")
+        self.alert("AVD is up")
         self.deploy()
 
     def deploy(self):
         self.verbose("Installing APK...")
-        #self.avd.install(self.apk.filename)
+        self.avd.install(self.apk.filename)
         self.avd.remount()
         self.avd.push("./libs/busybox-android/busybox-android", "/system/xbin/busybox")
         self.avd.push("./libs/busybox-android/android-remote-install.sh", "/system/bin/android-remote-install.sh")
+        self.avd.shell("chmod 777 /system/bin/android-remote-install.sh")
         self.avd.shell("/system/bin/android-remote-install.sh")
 
-        #TODO: start logcat, proxy and network capture, depending on provided options
+        self.verbose("Launching application ...")
         for a in self.get_activities():
             if a["exported"] and len(a["intent_filters"]) and \
                             a["intent_filters"][0]["action"] == "android.intent.action.MAIN" \
                     and a["intent_filters"][0]["category"] == "android.intent.category.LAUNCHER":
-                cmd = "am start %s/%s" % (self.apk.get_package(), a["name"])
-                self.avd.shell(cmd)
+                cmd = "am start -n %s/%s" % (self.apk.get_package(), a["name"])
+                output = self.avd.shell(cmd)
 
     def analyze(self, arguments):
+        """
+
+        :param arguments:
+        :return:
+        """
         self.analysis["start_time"] = int(time.time())
         self.analysis["application"]["package"] = self.apk.package
         self.analysis["application"]["path"] = self.apk.filename
         self.analysis["application"]["name"] = self.apk.filename
         self.analysis["application"]["size"] = os.path.getsize(self.apk.filename)
 
-        self.avd = self.find_avd()
-        if not self.avd.isrunning:
-            self.avd.launch(self.on_boot)
-        else:
-            self.deploy()
+        logcat_pid1 = 0
+        logcat_pid2 = 0
+
         self.fry()
-        self.static_analysis(arguments.module if arguments.module else None)
+        if not arguments.static_only:
+            self.verbose("Searching AVD ...")
+            self.avd = self.find_avd()
+            self.verbose("AVD found")
+            if not self.avd.isrunning:
+                self.verbose("Launching AVD")
+                self.avd.launch(self.on_boot, headless=True)
+            else:
+                self.deploy()
 
-        #self.report()
-        #Start dynamic analysis
-        #self.dynamic_analysis(avd)
-        #self.teleport(avd)
+            self.verbose("Setting up logcat logger...")
+            self.subprocesses.append(self.avd.logcat("analysis/%s/logcat_pkg.log" % self.apk.get_package(), tag=self.apk.get_package()))
+            self.subprocesses.append(self.avd.logcat("analysis/%s/logcat_full.log" % self.apk.get_package(), tag=None))
+            self.verbose("Launching network capture ...")
+            self.avd.start_traffic_capture("analysis/%s/capture_%d.pcap" % (self.apk.get_package(), int(time.time())))
 
-        #TODO: teleporting + traffic analysis
+        try:
+            if arguments.module is not None:
+                modules = [x for x in self.loaded_modules if x == arguments.module and x.startswith("static")]
+            else:
+                modules = [x for x in self.loaded_modules if x.startswith("static")]
 
-        #1. open pcap file with scapy
-        #pcap = rdpcap('./analysis/%s/file.pcap' % self.apk.get_package())
-        #2. perform analysis
+            for k in modules:
+                m = sys.modules[self.loaded_modules[k]].Module(self.apk, self.avd)
+                self.verbose("Running %s ..." % (m.info['Name']))
+                r = {
+                    "start_time": int(time.time()),
+                    "name": m.info["Name"],
+                    "run": m.module_run(),
+                    "end_time": int(time.time())
+                }
+                for v in r["run"]["vulnerabilities"]:
+                    self.alert(v["name"])
+                self.analysis["modules"][k] = r
 
-        #TODO: define analysis vectors
-        # timing analysis, list of target IPs, amount of transmitted data ?
+        except Exception as e:
+            self.error(str(e))
+
+        if self.avd is not None:
+            self.teleport(self.avd)
+            #1. open pcap file with scapy
+            #pcap = rdpcap('./analysis/%s/file.pcap' % self.apk.get_package())
+            #2. perform analysis
+            #TODO: define analysis vectors
+            # timing analysis, list of target IPs, amount of transmitted data ?
+
+            for pid in self.subprocesses:
+                os.kill(pid+1, signal.SIGTERM)
+            self.verbose("Stopping network capture ...")
+            self.avd.stop_traffic_capture()
+            self.verbose("Uninstalling APK ...")
+            print self.avd.uninstall(self.apk.get_package())
+            #self.verbose("Shutting down AVD...")
+            #self.avd.shutdown()
         self.analysis["end_time"] = int(time.time())
+        self.report()
 
     def load_apk(self):
         """
@@ -135,9 +179,9 @@ class AAAP(framework.module):
                     self.apk = a
                     self.manifest = self.apk.get_android_manifest_xml().getElementsByTagName("manifest")[0]
                 else:
-                    self.error("Invalid APK")
+                    self.error("The APK file you provided is not valid.")
             else:
-                self.error("Invalid APK")
+                self.error("The APK file you provided is not valid.")
         except Exception as e:
             self.error(str(e))
 
@@ -221,7 +265,7 @@ class AAAP(framework.module):
             #binary xml values.
             self.verbose("Converting Application Manifest to human readable format ...")
             with open("./analysis/%s/orig/AndroidManifest.xml" % self.apk.get_package(), "w") as f:
-                f.write(self.apk.get_android_manifest_xml().toprettyxml())
+                f.write(self.apk.get_android_manifest_xml().toprettyxml().decode('utf-8'))
 
         except Exception as e:
             self.error(str(e))
@@ -230,70 +274,54 @@ class AAAP(framework.module):
         """Copy all application related files from the device to the analysis directory for further analysis.
         """
 
-        if not os.path.exists("./analysis/%s/device/data/data/%s" % (self.apk.get_package(), self.apk.get_package())):
+        if not os.path.exists("./analysis/%s" % (self.apk.get_package())):
+            os.mkdir("./analysis/%s" % self.apk.get_package())
+        if not os.path.exists("./analysis/%s/device" % (self.apk.get_package())):
             os.mkdir("./analysis/%s/device/" % self.apk.get_package())
+        if not os.path.exists("./analysis/%s/device/sdcard" % (self.apk.get_package())):
+            os.mkdir("./analysis/%s/device/sdcard" % self.apk.get_package())
+        if not os.path.exists("./analysis/%s/device/data" % (self.apk.get_package())):
             os.mkdir("./analysis/%s/device/data" % self.apk.get_package())
+        if not os.path.exists("./analysis/%s/device/data/data" % (self.apk.get_package())):
             os.mkdir("./analysis/%s/device/data/data" % self.apk.get_package())
+        if not os.path.exists("./analysis/%s/device/data/data/%s" % (self.apk.get_package(), self.apk.get_package())):
             os.mkdir("./analysis/%s/device/data/data/%s" % (self.apk.get_package(), self.apk.get_package()))
+        if not os.path.exists("./analysis/%s/device/data/system" % (self.apk.get_package())):
+            os.mkdir("./analysis/%s/device/data/system" % (self.apk.get_package()))
 
         source = "/data/data/%s" % self.apk.get_package()
         dest = "./analysis/%s/device/data/data" % self.apk.get_package()
         avd.pull(source, dest)
-        self.verbose("Teleporting data from %s to %s" % (source, dest))
+
+        self.verbose("Teleporting data ...")
 
         #search files owned by the application's user (u0_a46) in the sdcard mount point.
         #1. get application UID
-        '''uid = None
-        avd.pull("/data/system/packages.list", "./analysis/%s/device/data/system" % self.apk.get_package())
-        with open("./analysis/%s/device/data/system" % self.apk.get_package(), "rb") as f:
+        uid = None
+        avd.pull("/data/system/packages.list", "./analysis/%s/device/data/system/" % self.apk.get_package())
+        with open("./analysis/%s/device/data/system/packages.list" % self.apk.get_package(), "rb") as f:
             for line in f.readlines():
                 if line.startswith(self.apk.get_package()):
                     uid = int(line.split(" ")[1])
-        self.verbose("Found application UID : %d" % uid)
+
         if uid is not None:
+            self.verbose("Found application UID : %d" % uid)
             self.verbose("Searching for files owned by user %d" % uid)
-            files = avd.shell("find -type f -user %d /" % uid)
-            for f in files.split("\n"):
-                avd.pull(f, "./analysis/%s/device/sdcard/" % self.apk.get_package())'''
+            files = avd.shell("find /sdcard -type f -user %d" % uid)
+            for f in [x for x in files.split("\n") if len(x)]:
+                print avd.pull(f, "./analysis/%s/device/sdcard" % self.apk.get_package())
         return
-
-    def static_analysis(self, module=None):
-        """Load and run each static analysis module.
-        Params:
-            module(string): a string identifying a static analysis module (e.g static/app/info)
-        Returns:
-        Throws:
-        """
-        try:
-            if module is not None:
-                modules = [x for x in self.loaded_modules if x == module and x.startswith("static")]
-            else:
-                modules = [x for x in self.loaded_modules if x.startswith("static")]
-
-            for k in modules:
-                m = sys.modules[self.loaded_modules[k]].Module(self.apk, self.avd)
-                self.verbose("Running %s ..." % (m.info['Name']))
-                r = {
-                    "start_time": int(time.time()),
-                    "name": m.info["Name"],
-                    "run": m.module_run(),
-                    "end_time": int(time.time())
-                }
-                for v in r["run"]["vulnerabilities"]:
-                    self.alert(v.name)
-                self.analysis["modules"][k] = r
-
-        except Exception as e:
-            self.error(str(e))
 
     def find_avd(self):
         try:
             for avd in Android.get_running_devices():
-                if int(self.apk.get_min_sdk_version()) <= avd.target <= int(self.apk.get_target_sdk_version()):
+                if int(self.apk.get_min_sdk_version() or 3) <= avd.target <= \
+                        int(self.apk.get_target_sdk_version() or 21):
                     return avd
 
             for avd in Android.get_avds():
-                if int(self.apk.get_min_sdk_version()) <= avd.target <= int(self.apk.get_target_sdk_version()):
+                if int(self.apk.get_min_sdk_version() or 3) <= avd.target <= \
+                        int(self.apk.get_target_sdk_version() or 21):
                     return avd
 
             self.alert("AVD not found, searching for targets ...")
@@ -301,9 +329,7 @@ class AAAP(framework.module):
             targets.reverse()
             t = None
             for target in targets:
-                #Google targets are even numbered
-                if int(self.apk.get_min_sdk_version()) <= target.api_level <= int(self.apk.get_target_sdk_version())\
-                        and not target.api_level % 2:
+                if int(self.apk.get_min_sdk_version()) <= target.api_level <= int(self.apk.get_target_sdk_version()):
                     self.alert("Found target : %s - %d" % (target.name, target.api_level))
                     t = target
                     break
@@ -312,64 +338,33 @@ class AAAP(framework.module):
                 p = subprocess.Popen(
                     "android update sdk -u -t %s" %
                     ",".join([str(x) for x in xrange(int(self.apk.get_min_sdk_version()),
-                                                     int(self.apk.get_target_sdk_version()))]),
+                                                     int(self.apk.get_target_sdk_version())+1)]),
                     shell=True,
+                    stdin=subprocess.PIPE,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE
                 )
+                p.stdin.write('y')
                 stdout, stderr = p.communicate()
                 if stderr:
                     raise Exception(stderr)
                 else:
-                    self.alert("Necessary target installed.")
-            else:
-                #NOTE: choosing default device, might be a better solution
-                self.alert("Creating AVD... [%s, %s, %s]" % (Android.get_devices()[0].id, t.api_level, t.skins.split(",")[0]))
+                    if "Unknown Host" in stdout:
+                        self.error("Missing internet connectivity. Aborting...")
+                    else:
+                        self.alert("Necessary target installed.")
+                        for target in targets:
+                            if int(self.apk.get_min_sdk_version()) <= target.api_level <= \
+                                    int(self.apk.get_target_sdk_version()):
+                                t = target
+                            break
+
+            #NOTE: choosing default device, might be a better solution
+            self.alert("Creating AVD... [%s, %s, %s]" % (Android.get_devices()[0].id, t.api_level, t.skins.split(",")[0]))
+            name = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
+            while name in [avd.name for avd in Android.get_avds()]:
                 name = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
-                while name in [avd.name for avd in Android.get_avds()]:
-                    name = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
-                return AVD.create(name, t.id,  Android.get_devices()[0].id)
-        except Exception as e:
-            self.error(str(e))
-
-    def dynamic_analysis(self, avd):
-        """Load and run each dynamic analysis module.
-        Params:
-            module(string): a string identifying a static analysis module (e.g dynamic/app/info)
-        Params:
-        Returns:
-        Throws:
-        """
-        def on_boot_finished(self, avd):
-            self.alert("Boot finished")
-            avd.install(self.apk_filename)
-            logcat_pid1 = avd.logcat("analysis/%s/logcat_pkg.txt" % self.apk.get_package(), tag=self.apk.get_package())
-            logcat_pid2 = avd.logcat("analysis/%s/logcat_full.txt" % self.apk.get_package(), tag=None)
-            self.verbose("Logcat running ...")
-            avd.start_traffic_capture("analysis/%s/capture_%d.pcap" % (self.apk.get_package(), int(time.time())))
-            self.verbose("Network capture running ...")
-            self.verbose("Setting up proxy ...")
-
-            self.verbose("Proxy is up")
-            print "Press enter when you are done testing the application."
-            raw_input("Press Enter to continue...")
-
-            os.kill(logcat_pid1, signal.SIGTERM)
-            os.kill(logcat_pid2, signal.SIGTERM)
-            self.verbose("Logcat killed.")
-            avd.stop_traffic_capture()
-            self.verbose("Network capture stopped.")
-            self.verbose("Analyzing results ...")
-            try:
-                for k in [x for x in self.loaded_modules if x.startswith("dynamic/")]:
-                    m = sys.modules[self.loaded_modules[k]].Module(self.apk)
-                    self.verbose("Running %s ..." % (m.info['Name']))
-                    self.analysis["modules"][k] = m.module_run(avd)
-            except Exception as e:
-                self.error(str(e))
-
-        try:
-            avd.launch(callback=on_boot_finished)
+            return AVD.create(name, t.id, Android.get_devices()[0].id, tag_abi=t.tag_abis[0], sdcard="512M")
         except Exception as e:
             self.error(str(e))
 
