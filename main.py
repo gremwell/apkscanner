8caa6d77ac8159fd22595f1de0972d25ded6f38b
Quentin Kaiser <kaiserquentin@gmail.com>
2015-03-09 14:56:14 +0100

    Add module, static-only and verbose CLI options.

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import sys
 import argparse
-
+import os
 sys.path.append('./core/')
-import base
+from base import APKScanner, __author__, __email__, __version__
 
 
 def main(arguments):
 
-    x = base.AAAP(arguments.apk)
-    x.analyze(arguments)
+    apks = APKScanner(arguments)
+    apks.analyze(module=arguments.module)
 
-description = '%%(prog)s - %s %s' % (base.__author__, base.__email__)
-parser = argparse.ArgumentParser(description=description, version=base.__version__)
+description = '%%(prog)s - %s %s' % (__author__, __email__)
+parser = argparse.ArgumentParser(description=description, version=__version__)
 parser.add_argument("apk")
-parser.add_argument("--module")
-parser.add_argument('-s', '--static-only', action="store_true")
+parser.add_argument("--module", help="run the provided module only")
+parser.add_argument("--static-only", help="rely only on static analysis", action="store_true")
+parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
+
 main(args)
