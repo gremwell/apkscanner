497c355104ed920ce28bf03ca9a120c9abad12e5
Quentin Kaiser <kaiserquentin@gmail.com>
2015-03-23 13:01:24 +0100

    Add script to setup Android SDK and NDK dependencies.

---
+++ install_requirements.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ANDROID_SDK="http://dl.google.com/android/android-sdk_r24.0.2-linux.tgz"
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	ANDROID_NDK="http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin"
+else
+	ANDROID_NDK="http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86.bin" 
+fi
+
+echo "[+] Installing Android SDK ..."
+wget $ANDROID_SDK -O /tmp/android.tgz
+tar xzvf /tmp/android.tgz -C ~
+
+echo "[+] Installing Android NDK ..."
+wget $ANDROID_NDK -O /tmp/android_ndk.bin
+chmod a+x /tmp/android_ndk.bin
+/tmp/android_ndk.bin
+mv android-ndk-r10d ~
+echo 'export PATH=$PATH:~/android-sdk-linux:~/android-ndk-r10d'  >> ~/.profile
