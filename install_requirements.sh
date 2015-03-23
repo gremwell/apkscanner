1d883cc3dd31fbbb9dd123ca244959fd8e8e63ee
Quentin Kaiser <kaiserquentin@gmail.com>
2015-03-23 15:10:20 +0100

    Verify if already installed. Setup PATH correctly.

--- install_requirements.sh
+++ install_requirements.sh
@@ -8,13 +8,17 @@ else
 	ANDROID_NDK="http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86.bin" 
 fi
 
-echo "[+] Installing Android SDK ..."
-wget $ANDROID_SDK -O /tmp/android.tgz
-tar xzvf /tmp/android.tgz -C ~
+if [ -z `which android` ]; then
+	echo "[+] Installing Android SDK ..."
+	wget $ANDROID_SDK -O /tmp/android.tgz
+	tar xzvf /tmp/android.tgz -C ~
+fi
 
-echo "[+] Installing Android NDK ..."
-wget $ANDROID_NDK -O /tmp/android_ndk.bin
-chmod a+x /tmp/android_ndk.bin
-/tmp/android_ndk.bin
-mv android-ndk-r10d ~
-echo 'export PATH=$PATH:~/android-sdk-linux:~/android-ndk-r10d'  >> ~/.profile
+if [ -z `which ndk-build` ]; then
+	echo "[+] Installing Android NDK ..."
+	wget $ANDROID_NDK -O /tmp/android_ndk.bin
+	chmod a+x /tmp/android_ndk.bin
+	/tmp/android_ndk.bin
+	mv android-ndk-r10d ~
+fi
+echo 'export PATH=$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/android-ndk-r10d'  >> ~/.profile
