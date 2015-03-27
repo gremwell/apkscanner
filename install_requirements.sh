de02e0a113b01c96f55782131ad4026361453475
Quentin Kaiser <kaiserquentin@gmail.com>
2015-03-27 14:38:31 +0100

    Simplified android update command.

--- install_requirements.sh
+++ install_requirements.sh
@@ -13,19 +13,17 @@ fi
 
 if [ -z `which android` ]; then
 	echo "[+] Installing Android SDK ..."
-	wget $ANDROID_SDK -O /tmp/android.tgz
+	wget ${ANDROID_SDK} -O /tmp/android.tgz
 	tar xzvf /tmp/android.tgz -C ~
 fi
 
 if [ -z `which ndk-build` ]; then
 	echo "[+] Installing Android NDK ..."
-	wget $ANDROID_NDK -O /tmp/android_ndk.bin
+	wget ${ANDROID_NDK} -O /tmp/android_ndk.bin
 	chmod a+x /tmp/android_ndk.bin
 	/tmp/android_ndk.bin
 	mv android-ndk-r10d ~
 fi
 echo 'export PATH=$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/android-ndk-r10d'  >> ~/.profile
 source ~/.profile
-while [ 1 ]; do sleep 1; echo y; done | android update sdk -u -a -t platform-tools
-while [ 1 ]; do sleep 1; echo y; done | android update sdk -u -a -t system-image
-while [ 1 ]; do sleep 1; echo y; done | android update sdk -u -a -t tools
+while [ 1 ]; do sleep 1; echo y; done | android update sdk -u -a -t tools,platform-tools,system-image
