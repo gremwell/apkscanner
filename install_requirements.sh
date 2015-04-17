466d1068f1d925adee90e1b383bd136912fcca06
Quentin Kaiser <kaiserquentin@gmail.com>
2015-04-17 11:43:44 +0200

    Fix requirements install.

--- install_requirements.sh
+++ install_requirements.sh
@@ -15,6 +15,7 @@ if [ -z `which android` ]; then
 	echo "[+] Installing Android SDK ..."
 	wget ${ANDROID_SDK} -O /tmp/android.tgz
 	tar xzvf /tmp/android.tgz -C ~
+	rm /tmp/android.tgz
 fi
 
 if [ -z `which ndk-build` ]; then
@@ -23,14 +24,16 @@ if [ -z `which ndk-build` ]; then
 	chmod a+x /tmp/android_ndk.bin
 	/tmp/android_ndk.bin
 	mv android-ndk-r10d ~
+	rm /tmp/android_ndk.bin
 fi
 echo 'export PATH=$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools:~/android-ndk-r10d'  >> ~/.profile
 source ~/.profile
 system_image_arm=`android list sdk -a -e | egrep "sys-img-armeabi-v7a-android-[0-9]+" | head -n1 | cut -d' ' -f2`
 system_image_x86=`android list sdk -a -e | egrep "sys-img-x86-android-[0-9]+" | head -n1 | cut -d' ' -f2`
 system_image_x86_64=`android list sdk -a -e | egrep "sys-img-x86_64-android-[0-9]+" | head -n1 | cut -d' ' -f2`
-system_image_mips=`android list sdk -a -e | egrep "sys-img-mips-android-[0-9]+" | head -n1 | cut -d' ' -f2`
+platform_tools=`android list sdk -a -e | grep "platform-tools" | cut -d' ' -f2`
 sdk_tools=`android list sdk -a -e | grep "\"tools\"" | cut -d' ' -f2`
 build_tools=`android list sdk -a -e | grep "build-tools" | head -n1 | cut -d' ' -f2`
-#APIS=` android list sdk -a -e | egrep "\"android-[0-9]+" | cut -d' ' -f2 | tr '\n' ','`
-while [ 1 ]; do sleep 1; echo y; done | android update sdk -u -a -t $sdk_tools,$build_tools,$system_image_arm,$system_image_x86,$system_image_x86_64,$system_image_mips
+platforms=`android list sdk -a -e | grep -E "android-[0-9]{1,2}" | head -n1 | cut -d' ' -f2`
+echo $platforms
+while [ 1 ]; do sleep 1; echo y; done | android update sdk -u -a -t $platform_tools,$build_tools,$sdk_tools,$platforms,$system_image_arm,$system_image_x86,$system_image_x86_64
