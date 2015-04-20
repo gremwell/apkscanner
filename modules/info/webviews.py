7657206b2f4251bcc7a5906093ed15d7a7a64e99
Quentin Kaiser <kaiserquentin@gmail.com>
2015-04-20 15:15:22 +0200

    Report only when js is enabled but cover all Webview settings that could raise issues if js is enabled.

--- modules/info/webviews.py
+++ modules/info/webviews.py
@@ -5,20 +5,6 @@ from androguard.core.analysis.analysis import *
 from androguard.decompiler.dad import decompile
 import re
 
-'''
-Method                                  Default value           Implication of being enabled
-
-setAllowContentAccess                   true                    WebView has access to content providers on the system
-setAllowFileAccess                      true                    Allows a WebView to load content from the filesystem using file:// scheme
-setAllowFileAccessFromFileURLS          true (<= API 15)        Allows the HTML file that was loaded using file:// scheme to access
-                                                                other files on the system
-setAllowUniversalAccessFromFilesURLS    true (<= API 15)        Allows the HTML file that was loaded using file:// to
-                                                                access content from any origin (including other files).
-setJavascriptEnabled                    false                   Allows the WebView to execute Javascript
-setPluginState                          PluginState.OFF         Allow the loading of plugins (ie. Flash)
-setSavePassword (deprecated as API18)   true                    The WebView will save passwords entered
-'''
-
 
 class Module(framework.module):
     def __init__(self, apk, avd):
@@ -32,91 +18,84 @@ class Module(framework.module):
 
     def module_run(self, verbose=False):
 
-        webviews = {
-            "javascript": [],
-            "plugin": []
-        }
         vulnerabilities = []
+        webviews = []
+
+        funcs = [
+            {
+                "name": "setJavaScriptEnabled",
+                "default": False,
+                "description": "Allows the WebView to execute Javascript."
+            },
+            {
+                "name": "setPluginsEnabled" if self.apk.get_min_sdk_version() < 8 else "setPluginState",
+                "default": True,
+                "description": "Allow the loading of plugins (ie. Flash)."
+            },
+            {
+                "name": "setAllowContentAccess",
+                "default": True,
+                "description": "WebView has access to content providers on the system."
+            },
+            {
+                "name": "setAllowFileAccess",
+                "default": True,
+                "description": "Allows a WebView to load content from the filesystem using file:// scheme."
+            },
+            {
+                "name": "setAllowFileAccessFromFileURLS",
+                "default": True if self.apk.get_min_sdk_version() <= 15 else False,
+                "description": "Allows the HTML file that was loaded using file:// scheme to access "
+                               "other files on the system"
+            },
+            {
+                "name": "setAllowUniversalAccessFromFilesURLS",
+                "default": True if self.apk.get_min_sdk_version() <= 15 else False,
+                "description": "Allows the HTML file that was loaded using file:// to "
+                               "access content from any origin (including other files)."
+            },
+            {
+                "name": "setSavePassword",
+                "default": True,
+                "description": "The WebView will save entered passwords."
+            }
+        ]
 
         d = dvm.DalvikVMFormat(self.apk.get_dex())
         dx = VMAnalysis(d)
-
-
-        z = dx.tainted_packages.search_methods(".", "setJavaScriptEnabled", ".")
-        for p in z:
-            method = d.get_method_by_idx(p.get_src_idx())
-            if method.get_code() is None:
-                continue
-            mx = dx.get_method(method)
-            ms = decompile.DvMethod(mx)
-            ms.process()
-            source = ms.get_source()
-
-            matches = re.findall(r'setJavaScriptEnabled\((1|true)\)', source)
-            if len(matches) == 1:
-                if method.get_class_name()[1:-1] not in [x["file"] for x in webviews["javascript"]]:
-                    webviews["javascript"].append({
-                        "file": method.get_class_name()[1:-1],
-                        "lines": [method.get_debug().get_line_start()]
-                    })
-                else:
-                    for w in webviews["javascript"]:
-                        if w["file"] == method.get_class_name()[1:-1]:
-                            if method.get_debug().get_line_start() not in w["lines"]:
-                                w["lines"].append(method.get_debug().get_line_start())
-
-        if self.apk.get_min_sdk_version() < 8:
-            z = dx.tainted_packages.search_methods(".", "setPluginsEnabled", ".")
-        else:
-            z = dx.tainted_packages.search_methods(".", "setPluginState", ".")
+        z = dx.tainted_packages.search_packages("WebView")
 
         for p in z:
             method = d.get_method_by_idx(p.get_src_idx())
             if method.get_code() is None:
                 continue
-            mx = dx.get_method(method)
-            ms = decompile.DvMethod(mx)
-            ms.process()
-            source = ms.get_source()
+            if method.get_class_name()[1:-1] not in [x["file"] for x in webviews]:
+                webview = {
+                    "file": method.get_class_name()[1:-1],
+                    "line": method.get_debug().get_line_start()
+                }
+                mx = dx.get_method(method)
+                ms = decompile.DvMethod(mx)
+                ms.process()
+                source = ms.get_source()
 
-            if self.apk.get_min_sdk_version() < 8:
-                matches = re.findall(r'setPluginsEnabled\((.*?)\)', source)
-            else:
-                matches = re.findall(r'setPluginState\((.*?)\)', source)
+                for func in funcs:
+                    matches = re.findall(r'%s\((.*?)\);' % func["name"], source)
+                    if len(matches) == 1:
+                        webview[func["name"]] = True if matches[0] == "1" or matches[0] == "true" else False
+                    else:
+                        webview[func["name"]] = func["default"]
+                webviews.append(webview)
 
-            if len(matches) == 1 and "ON" in matches[0]:
-                if method.get_class_name()[1:-1] not in [x["file"] for x in webviews["plugin"]]:
-                    webviews["plugin"].append({
-                        "file": method.get_class_name()[1:-1],
-                        "lines": [method.get_debug().get_line_start()]
-                    })
-                else:
-                    for w in webviews["plugin"]:
-                        if w["file"] == method.get_class_name()[1:-1]:
-                            if method.get_debug().get_line_start() not in w["lines"]:
-                                w["lines"].append(method.get_debug().get_line_start())
+        for webview in webviews:
+            if webview["setJavaScriptEnabled"]:
+                vulnerabilities.append(framework.Vulnerability(
+                    "Explicitly enabled Javascript in WebViews",
+                    "The application explicitly enable Javascript for multiple webviews.",
+                    framework.Vulnerability.LOW
+                ).__dict__)
 
-
-
-        if len(webviews["plugin"]) and len(webviews["javascript"]):
-            vulnerabilities.append(framework.Vulnerability(
-                "Explicitly enabled Javascript and Plugins in WebViews",
-                "The application explicitly enable Javascript and Plugins for multiple webviews.",
-                framework.Vulnerability.LOW
-            ).__dict__)
-        elif len(webviews["javascript"]) and not len(webviews["plugin"]):
-            vulnerabilities.append(framework.Vulnerability(
-                "Explicitly enabled Javascript in WebViews",
-                "The application explicitly enable Javascript for multiple webviews.",
-                framework.Vulnerability.LOW
-            ).__dict__)
-        elif not len(webviews["javascript"]) and len(webviews["plugin"]):
-            vulnerabilities.append(framework.Vulnerability(
-                "Explicitly enabled Plugins in WebViews",
-                "The application explicitly enable Plugins for multiple webviews.",
-                framework.Vulnerability.LOW
-            ).__dict__)
-
+        print vulnerabilities
         return {
             "results": webviews,
             "logs": "",
