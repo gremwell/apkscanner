93d6494a5879abaa974dff5daf973cb94595c974
Quentin Kaiser <kaiserquentin@gmail.com>
2015-05-11 17:20:41 +0200

    Search for sensitive API calls with Androguard search_packages. Might need addition of packages.

---
+++ modules/info/apis.py
@@ -0,0 +1,65 @@
+import framework
+from androguard.core.bytecodes import dvm
+from androguard.core.analysis.analysis import *
+from androguard.decompiler.dad import decompile
+
+
+class Module(framework.module):
+    def __init__(self, apk, avd):
+        super(Module, self).__init__(apk, avd)
+        self.info = {
+            'Name': 'Android API usage',
+            'Author': 'Quentin Kaiser (@QKaiser)',
+            'Description': 'This module will find usage of sensitive Android APIs '
+                           '(file, network, crypto, communications) within the code.',
+            'Comments': [
+                ""
+            ]
+        }
+
+    def module_run(self, verbose=False):
+
+        results = {}
+        apis = {
+            "crypto": [ "javax.crypto", "java.security", "android.security"],
+            "networking": ["java.net", "org.apache.http", "javax.net", "javax.net.ssl"],
+            "IO": ["java.io"],
+            "comm": ["android.bluetooth", "android.net.sip", "android.net.wifi", "android.net.wifi.p2p", "android.nfc"],
+            "geolocation": ["android.location"]
+        }
+        d = dvm.DalvikVMFormat(self.apk.get_dex())
+        dx = VMAnalysis(d)
+
+        for api in apis:
+            results[api] = {}
+            if verbose:
+                self.output("Searching for %s calls" % api)
+                for package in apis[api]:
+                    results[api][package] = {}
+                    z = dx.tainted_packages.search_packages(package)
+                    for p in z:
+                        method = d.get_method_by_idx(p.get_src_idx())
+                        if self.apk.package.replace(".", "/") in method.get_class_name()[1:-1]:
+                            if method.get_code() is None:
+                                continue
+                            mx = dx.get_method(method)
+                            ms = decompile.DvMethod(mx)
+                            ms.process()
+
+                            if method.get_class_name()[1:-1] not in results[api][package]:
+                                results[api][package][method.get_class_name()[1:-1]] = []
+
+                            if method.get_debug().get_line_start() not in \
+                                    [x["line"] for x in results[api][package][method.get_class_name()[1:-1]]]:
+                                results[api][package][method.get_class_name()[1:-1]].append(
+                                    {
+                                        "line": method.get_debug().get_line_start()
+                                    }
+                                )
+        if verbose:
+            print results
+        return {
+            "results": results,
+            "logs": "",
+            "vulnerabilities": []
+        }
